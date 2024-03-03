b932e8169e27b60d3bf73273de92795a71fe50c5
rrazban <rmr4442@gmail.com>
2024-03-03 10:14:50 -0500

    correct ukb ages to reflect scan date

--- ukb/ukb.m
+++ ukb/ukb.m
@@ -1,9 +1,9 @@
 function [title_text, age_bins, data_dir, ages, subs, correct_T, Neff_opt, threshold, exact_norm_thresh]=ukb(parcellation)
 
 title_text ='UK Biobank';
-age_bins = 40:5:70;
+age_bins = 45:5:80;	
 
-data_dir = strcat('/shared/datasets/public/ukb/derivatives/parcelled/', parcellation);	%300
+data_dir = strcat('/shared/datasets/public/ukb/derivatives/parcelled/', parcellation,'/001hz/');	%300
 
 info_file = 'phenotypes.csv';
 info = tdfread(info_file,',');
